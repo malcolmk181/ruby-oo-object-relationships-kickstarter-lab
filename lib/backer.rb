aea05efdf93c63f8b7b8b96be7d6d1d075331116
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-02 14:19:50 -0400

    commit tree 998520efc210f2186eca07f21f2f0fa46285f9c9

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+class Backer
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        ProjectBacker.all.select {|pb| pb.backer == self }.map {|pb| pb.project }
+    end
+end
